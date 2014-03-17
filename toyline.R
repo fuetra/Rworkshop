024013890d8d3687172c6a4c501490507e8b1f4d
Tracy Fuentes <tfuentes@uw.edu>
2014-03-17 13:56:11 -0700

    Toyline plot data and figure

---
+++ toyline.R
@@ -0,0 +1,16 @@
+a <- 2
+b <- -3
+sigSq <- 0.5
+x  <- runif(40)
+y  <- a + b*x + rnorm(40, sd=sqrt(sigSq))
+plot(x,y)
+abline(a,b, col="red")
+dev.print(pdf, "toylinePlot.pdf")
+a <- 2
+b <- -3
+sigSq <- 0.5
+x  <- runif(40)
+y  <- a + b*x + rnorm(40, sd=sqrt(sigSq))
+plot(x,y)
+abline(a,b, col="red")
+dev.print(pdf, "toylinePlot.pdf")
